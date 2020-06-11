a73eef97b2f5318af81cc45c8295354e57d9783d
cnmr_cb <cristhian.mayor.capgemini@servier.com>
2020-06-11 14:56:39 +0200

    server & client code

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+var client = require("./elasticOperations/client.js");
+var constants = require("./elasticOperations/constants.js");
+var executeCreateIndex = require("./elasticOperations/createIndex.js");
+var executeAddMapping = require("./elasticOperations/addMapping.js");
+var executeInsert = require("./elasticOperations/insertData.js");
+var executeSearch = require("./elasticOperations/searchData.js");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.route("/api/createIndex").post(executeCreateIndex);
+app.route("/api/addMapping").post(executeAddMapping);
+app.route("/api/insertData").post(executeInsert);
+app.route("/api/searchData").get(executeSearch);
+
+
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
